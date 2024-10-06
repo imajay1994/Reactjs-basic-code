50891d9e23577c4211189f5199efd09a6eeba338
AJAY Anand <119491134+imajay1994@users.noreply.github.com>
2024-10-06 10:10:30 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function App() {
+  const [name,setName]=useState("Ajay");
+
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  )
+}
+
+export default App
